e27304968eeb9cccbea9595024ddbed733d12e54
Zaky1325 <zakyardi135@gmail.com>
2023-08-02 15:04:10 +0700

    update

--- test logika 2.php
+++ test logika 2.php
@@ -1,12 +1,15 @@
-<?php
-function hitung_jumlah_kata($kalimat) {
-    // Menghapus kata dengan karakter khusus menggunakan regular expression
-    $kalimat_bersih = preg_replace('/\b[A-Za-z0-9]*[^A-Za-z0-9\s][A-Za-z0-9]*\b/', '', $kalimat);
-
-    // Menghitung jumlah kata dalam kalimat yang telah dibersihkan
-    $jumlah_kata = str_word_count($kalimat_bersih);
-
-    return $jumlah_kata;
+`<?php
+	function hitung_jumlah_kata($kalimat) {
+    $jumlah = 0;
+    
+    foreach(explode(" ", $kalimat) as $string) {
+      if (!preg_match('/[*_(\[!=&]/', $string))
+      {
+          $jumlah += 1;
+      }
+    }
+    
+    return $jumlah;
 }
 
 $input_kalimat = "Saat meng*ecat tembok, Agung dib_antu oleh Raihan.";
@@ -19,7 +22,7 @@ $output = hitung_jumlah_kata($input_kalimat);
 echo "Output: " . $output . "\n";
 
 
-$input_kalimat = " Masing-masing anak mendap(atkan uang jajan ya=ng be&rbeda.
+$input_kalimat = "Masing-masing anak mendap(atkan uang jajan ya=ng be&rbeda.
 ";
 $output = hitung_jumlah_kata($input_kalimat);
 echo "Output: " . $output . "\n";
